3dcf84a7b931fc8a88b4e73a9636c4e979d91acf
ArthurLVidal <arthurodri2017@gmail.com>
2024-06-05 10:19:48 -0300

    Extraindo a grid dos produtos para um arquivo separado

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,35 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/components/product_item.dart';
-import 'package:myshop/models/product_list.dart';
-import 'package:provider/provider.dart';
-import '../models/product.dart';
+import 'package:myshop/components/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         actions: [],
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
