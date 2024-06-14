26bb3c928bc4901b70fb466080cd50eb8f8b926b
ArthurLVidal <arthurodri2017@gmail.com>
2024-06-14 11:14:49 -0300

    Editando isFavorite no banco de dados

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,12 +4,10 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:myshop/models/product.dart';
+import 'package:myshop/utils/constants.dart';
 import '../exceptions/http_exception.dart';
 
 class ProductList with ChangeNotifier {
-  final _baseUrl =
-      'https://shop-study-4a13e-default-rtdb.firebaseio.com/products';
-
   List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -22,7 +20,8 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse('$_baseUrl.json'));
+    final response =
+        await http.get(Uri.parse('${Constants.PRODUCT_BASE_URL}.json'));
 
     if (response.body == 'null') return;
 
@@ -44,7 +43,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl.json'),
+      Uri.parse('${Constants.PRODUCT_BASE_URL}.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -91,7 +90,7 @@ class ProductList with ChangeNotifier {
 
     if (index >= 0) {
       await http.patch(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
         body: jsonEncode(
           {
             "name": product.name,
@@ -115,7 +114,7 @@ class ProductList with ChangeNotifier {
       notifyListeners();
 
       final response = await http.delete(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
       );
 
       if (response.statusCode >= 400) {
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'https://shop-study-4a13e-default-rtdb.firebaseio.com/products';
+}
